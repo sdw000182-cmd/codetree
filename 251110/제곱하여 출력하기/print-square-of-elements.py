a264da73e7052deef30b3e47115edb68b9334fa4
석민호 <sdw000182@gmail.com>
2025-11-10 17:51:55 +0900

    Title: 제곱하여 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251110/제곱하여 출력하기/print-square-of-elements.py
+++ 251110/제곱하여 출력하기/print-square-of-elements.py
@@ -1,12 +1,8 @@
-# 변수 선언 및 입력
-n = int(input())
+N = int(input())
 
-# 배열에 주어진 수를 입력받아 저장합니다.
-arr = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
-# n개의 정수를 제곱한 배열을 새로 만듭니다.
-new_arr = [elem * elem for elem in arr]
+New_A = [elem * elem for elem in A]
 
-# n개의 정수를 출력합니다.
-for i in range(n):
-	print(new_arr[i], end=" ")
+for i in range(N):
+    print(New_A[i], end = " ")
