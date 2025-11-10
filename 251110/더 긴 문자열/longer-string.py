5044767744f52744260a6991d4989be54d3b1946
석민호 <sdw000182@gmail.com>
2025-11-10 20:06:30 +0900

    Title: 더 긴 문자열, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251110/더 긴 문자열/longer-string.py
+++ 251110/더 긴 문자열/longer-string.py
@@ -1,8 +1,8 @@
-a, b = input().split()
+str1, str2 = input().split()
 
 len1 = len(str1)
 len2 = len(str2)
-iif len1 > len2:
+if len1 > len2:
 	print(str1, len1)
 elif len1 < len2:
 	print(str2, len2)
